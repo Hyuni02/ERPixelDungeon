c0bc585e060d0b05da1076216bc4db6ea1012093
ErrorCode02 <jackjsh@naver.com>
2024-01-14 20:31:21 +0900

    냉면 화상제거 구현

--- Arrrknights/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/food/ColdNoodles.java
+++ Arrrknights/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/food/ColdNoodles.java
@@ -1,12 +1,20 @@
 package com.shatteredpixel.shatteredpixeldungeon.items.food;
 
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Burning;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Hunger;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
 
-public class ColdNoodles extends Food{
-
+public class ColdNoodles extends Food {
     {
         image = ItemSpriteSheet.PASTY; //todo 냉면 이미지 변경
         energy = Hunger.HUNGRY;
+    }
+
+    @Override
+    protected void satisfy(Hero hero) {
+        Buff.detach(hero, Burning.class);
+        super.satisfy(hero);
     }
 }
