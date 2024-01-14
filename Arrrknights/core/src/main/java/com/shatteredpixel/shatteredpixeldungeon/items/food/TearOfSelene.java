88905aa73af634f93d0869df2db603963e9de021
ErrorCode02 <jackjsh@naver.com>
2024-01-14 20:47:17 +0900

    셀레네의 눈물 구현

--- Arrrknights/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/food/TearOfSelene.java
+++ Arrrknights/core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/food/TearOfSelene.java
@@ -1,11 +1,19 @@
 package com.shatteredpixel.shatteredpixeldungeon.items.food;
 
+import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Hunger;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
 
 public class TearOfSelene extends Food{
     {
         image = ItemSpriteSheet.PASTY; //todo 셀레네의 눈물 이미지 변경
         energy = Hunger.HUNGRY/3;
+    }
+
+    @Override
+    protected void satisfy(Hero hero) {
+        Dungeon.hero.STR += 1;
+        super.satisfy(hero);
     }
 }
